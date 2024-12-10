9cd39def8b3bdcba9367a74dcfd8d4e55ce24f6e
Shikhar <ithinkitsright0152@gmail.com>
2024-12-10 21:13:57 +0530

    final touches

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -11,7 +11,7 @@ export function SendMoney() {
                         <div className="relative inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full dark:bg-green-400">
                             <div className="text-white text-xl">{name[0]}</div>
                         </div>
-                        <div className="flex items-center font-semibold text-xl">{name}</div>
+                        <div className="flex items-center font-semibold text-2xl">{name}</div>
                     </div>
                     <label className="font-semibold">Enter amount (in ₹)</label>
                     <input type="text" placeholder="Enter amount..." className="border-2 rounded-md px-2 py-1 mt-2 mb-6 w-full"/>
