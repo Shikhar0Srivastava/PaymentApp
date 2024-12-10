33cea2ea1caca1e00a165a816a2b51fb70fbc3fb
Shikhar <ithinkitsright0152@gmail.com>
2024-12-10 21:00:02 +0530

    Send money page finished. Designing over

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,5 +1,24 @@
 export function SendMoney() {
-    return <div>
-        send money
+    const name = "Shikhar"
+    return <div className="bg-slate-300 h-screen flex justify-center items-center">
+        <div className="border h-min max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg">
+            <div className="flex flex-col justify-center">
+                <div className="text-4xl font-bold p-6 text-center">
+                    Send Money            
+                </div>
+                <div className="p-6">
+                    <div className="flex space-x-4 mb-4">
+                        <div className="relative inline-flex items-center justify-center w-12 h-12 overflow-hidden bg-gray-100 rounded-full dark:bg-green-400">
+                            <div className="text-white text-xl">{name[0]}</div>
+                        </div>
+                        <div className="flex items-center font-semibold text-xl">{name}</div>
+                    </div>
+                    <label className="font-semibold">Enter amount (in ₹)</label>
+                    <input type="text" placeholder="Enter amount..." className="border-2 rounded-md px-2 py-1 mt-2 mb-6 w-full"/>
+
+                    <button className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white hover:bg-green-800">Click to send</button>
+                </div>
+            </div>
+        </div>
     </div>
 }
