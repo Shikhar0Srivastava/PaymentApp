190a3b69eec43cfc6be0b10fbabf5a1db2b282fb
Shikhar <ithinkitsright0152@gmail.com>
2024-12-17 10:58:00 +0530

    changed button appearance

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types"
 import { Loader } from "./Loader"
 
 export function Button({label, onClick, loading}) {
-    return <button onClick={onClick} disabled={loading} type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+    return <button onClick={onClick} disabled={loading} type="button" className="text-white w-full bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700" style={{backgroundColor: loading? "white" : "#363636"}}>
         {loading ? <Loader/>: label}
     </button>
 }
