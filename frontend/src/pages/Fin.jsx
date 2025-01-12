c98e696feaa5c2f8e0844e2037f56d09dd8959b6
Shikhar0Srivastava <77525984+Shikhar0Srivastava@users.noreply.github.com>
2025-01-12 12:03:35 +0530

    Update Fin.jsx

--- frontend/src/pages/Fin.jsx
+++ frontend/src/pages/Fin.jsx
@@ -4,8 +4,8 @@ import { Subheading } from "../components/Subheading"
 export function Fin() {
     return <div className="min-h-screen w-max-md bg-[#8476ba] flex justify-center items-center">
         <div className="border h-min p-16 space-y-8 w-120 bg-white shadow-lg rounded-lg">
-            <Heading label={"This page was made with ❤️"}></Heading>
-            <Subheading label={"(Not with madam Mehimi, although she's my love too)"}></Subheading>
+            <Heading label={"You've reached the end."}></Heading>
+            <Subheading label={"Transaction is done."}></Subheading>
         </div>
     </div>
 }
